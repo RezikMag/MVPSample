9732091798cd735a6fce39042a9a3b86cc0619c9
Rezik <Rizzvan_08@mail.ru>
2018-09-28 14:38:27 +0300

    simple guide mvp

---
+++ app/src/main/java/com/rezikmag/mvppattern/Repository/MainRepository.java
@@ -0,0 +1,20 @@
+package com.rezikmag.mvppattern.Repository;
+
+import android.util.Log;
+
+import com.rezikmag.mvppattern.MainContract;
+
+public class MainRepository implements MainContract.Repository {
+
+    private static final String TAG = "MainRepository";
+    @Override
+    public String loadMessage() {
+        Log.d(TAG, "loadMessage()");
+        /** Здесь обращаемся к БД или сети.
+         * Я специально ничего не пишу, чтобы не загромождать пример
+         * DBHelper'ами и прочими не относяшимеся к теме объектами.
+         * Поэтому я буду возвращать строку Сосисочная =)
+         */
+        return "Сосисочная у Лёхи";
+    }
+}
